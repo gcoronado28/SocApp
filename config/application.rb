394d2e10e75490c81654fa5339f13c9e397a5233
Gabriel Coronado <gabrielomar2809@gmail.com>
2019-07-09 10:35:52 -0500

    ENV variables managed & callback url added to devise.rb

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,14 @@ module Socapp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Set ENV variables from env_file.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end
+    end
+
   end
 end
